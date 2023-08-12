55fae7b03d919c0ead8ad9646ebe95cd5b7ab03c
naosense <pingao777@gmail.com>
2023-08-12 21:08:06 +0800

    extractor filter distance <= 0

--- extractor/garmin.py
+++ extractor/garmin.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 def parse_garmin_export_data() -> None:
     with open("Activities.csv") as source:
-        with open("../running.csv", "a") as target:
+        with open("running.csv", "a") as target:
             for line in reversed(list(source)):
                 cols = line.rstrip().split(",")
                 if cols[0] == "活动类型":
                     continue
-                target.write(cols[1] + "," + cols[4][1:-1] + "\n")
+                distance = cols[4][1:-1]
+                if float(distance) <= 0.0:
+                    continue
+                target.write(f"{cols[1]},{distance},{cols[7][1:-1]},{cols[11][1:-1]}\n")
 
 
 if __name__ == "__main__":
